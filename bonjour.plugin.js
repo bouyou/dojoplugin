3dc94a330428cfda62ac8fa1ef0b76802266b798
vanoverberghe <billycobam@hotmail.fr>
2024-05-23 18:59:50 +0200

    plugin ok

--- bonjour.plugin.js
+++ bonjour.plugin.js
@@ -1,16 +1,10 @@
-
 /**
- * @name bonjour
- * @author dojo
- * @description Describe the basic information. Maybe a support server link.
- * @version 0.0.1
- * @invite inviteCode
- * @authorId 51512151151651
- * @authorLink https://twitter.com/Whoever
- * @donate https://paypal.me/
- * @patreon https://patreon.com/
- * @website https://github.com/BetterDiscord/BetterDiscord
- * @source https://gist.github.com/rauenzi/e5f4d02fc3085a53872b0236cd6f8225
+ * @name Bonjour
+ * @version 1.7
+ * @description dojo
+ * @author toto
+ * @source https://github.com/An00nymushun/DiscordFreeEmojis
+ * @updateUrl https://raw.githubusercontent.com/An00nymushun/DiscordFreeEmojis/main/DiscordFreeEmojis64px.plugin.js
  */
 
 module.exports = class MyPlugin {
