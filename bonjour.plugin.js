5ee2ad63e496d78026fc373d547b835a74ce3147
vanoverberghe <billycobam@hotmail.fr>
2024-05-23 17:50:17 +0200

    add script basic +  meta

---
+++ bonjour.plugin.js
@@ -0,0 +1,23 @@
+
+/**
+ * @name bonjour
+ * @author dojo
+ * @description Describe the basic information. Maybe a support server link.
+ * @version 0.0.1
+ * @invite inviteCode
+ * @authorId 51512151151651
+ * @authorLink https://twitter.com/Whoever
+ * @donate https://paypal.me/
+ * @patreon https://patreon.com/
+ * @website https://github.com/BetterDiscord/BetterDiscord
+ * @source https://gist.github.com/rauenzi/e5f4d02fc3085a53872b0236cd6f8225
+ */
+
+module.exports = () => ({
+    start() {
+ 
+    },
+    stop() {
+ 
+    }
+ });
