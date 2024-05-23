327d10b1d972955bb17beca9fdf6034bf111e82c
vanoverberghe <billycobam@hotmail.fr>
2024-05-23 18:05:35 +0200

    add test hello world

--- bonjour.plugin.js
+++ bonjour.plugin.js
@@ -13,11 +13,23 @@
  * @source https://gist.github.com/rauenzi/e5f4d02fc3085a53872b0236cd6f8225
  */
 
-module.exports = () => ({
-    start() {
- 
-    },
-    stop() {
- 
+module.exports = class MyPlugin {
+    constructor(meta) {
+      // Do stuff in here before starting
     }
- });
+  
+    start() {
+        // Do stuff when enabled
+        const myButton = document.createElement("button");
+        myButton.textContent = "Click me!";
+        myButton.addEventListener("click", () => {window.alert("Hello World!");});
+        const root = document.getElementById("app-mount");
+        root.append(myButton);
+
+        BdApi.UI.alert("Hello World", "This is just a basic informational modal!");
+    }
+  
+    stop() {
+      // Cleanup when disabled
+    }
+  };
